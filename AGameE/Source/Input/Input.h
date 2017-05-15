d26d21c139d2f2f2c68b0f39d403ac0926ba694c
JIMICODE <1159081156@qq.com>
2017-05-15 19:29:00 +0800

    Keyboard and Mouse input.

--- AGameE/Source/Input/Input.h
+++ AGameE/Source/Input/Input.h
@@ -4,10 +4,19 @@
 
 #include<Xinput.h>
 #include<d3d12.h>
+#include<dinput.h>
 
 #pragma comment(lib, "xinput.lib")
+#pragma comment(lib, "dinput8.lib")
 
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
+
+LPDIRECTINPUT8 gDInput;
+LPDIRECTINPUTDEVICE8 gDIMouse;
+LPDIRECTINPUTDEVICE8 gDIKeyboard;
+DIMOUSESTATE gMouseSTate;
+XINPUT_GAMEPAD gControllers[4];
+char gKey[256];
 
 //Initialize XInput and any connected controllers
 bool XInput_Init(int contNum = 0)
@@ -84,6 +93,77 @@ void XInput_Update()
 			else if (state.Gamepad.wButtons & XINPUT_GAMEPAD_BACK)
 				break;
 		}
+	}
+}
+
+bool DirectInput_Init(HWND hwnd)
+{
+	//initialize DirectInput object;
+	HRESULT r = DirectInput8Create(
+		GetModuleHandle(NULL),
+		DIRECTINPUT_VERSION,
+		IID_IDirectInput8,
+		(void**)&gDInput,
+		NULL
+	);
+
+	//initialize the keyboard
+	gDInput->CreateDevice(GUID_SysKeyboard, &gDIKeyboard, NULL);
+	gDIKeyboard->SetDataFormat(&c_dfDIKeyboard);
+	gDIKeyboard->SetCooperativeLevel(hwnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND);
+	gDIKeyboard->Acquire();
+
+	//initialize the mouse
+	gDInput->CreateDevice(GUID_SysMouse, &gDIMouse, NULL);
+	gDIMouse->SetDataFormat(&c_dfDIMouse);
+	gDIMouse->SetCooperativeLevel(hwnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND);
+	gDIMouse->Acquire();
+	
+	return true;
+}
+
+void DirectInput_Update()
+{
+	//update mouse
+	gDIMouse->GetDeviceState(sizeof(gMouseSTate), (LPVOID)&gMouseSTate);
+	//update keyboard
+	gDIKeyboard->GetDeviceState(sizeof(gKey), (LPVOID)&gKey);
+}
+
+//return mouse x movement
+int MouseX()
+{
+	return gMouseSTate.lX;
+}
+//return mouse y movement
+int MouseY()
+{
+	return gMouseSTate.lY;
+}
+//return mouse button state
+int MouseButton(int button)
+{
+	return gMouseSTate.rgbButtons[button] & 0x80;
+}
+//return key press state
+int KeyDown(int key)
+{
+	return (gKey[key] & 0x80);
+}
+//DirectInput shutdown
+void DirectInput_Shutdown()
+{
+	if (gDIKeyboard)
+	{
+		gDIKeyboard->Unacquire();
+		gDIKeyboard->Release();
+		gDIKeyboard = NULL;
+	}
+	if (gDIMouse)
+	{
+		gDIMouse->Unacquire();
+		gDIMouse->Release();
+		gDIMouse = NULL;
 	}
 }
 
